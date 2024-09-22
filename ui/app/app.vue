7b5a99fba51c313ec6bf39bb9e8f118b27bfb725
ajiu9 <6159443232@qq.com>
2024-09-22 21:39:47 +0800

    feat: basic ui

---
+++ ui/app/app.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+useHead({
+  title: 'RegexDoctor',
+})
+</script>
+
+<template>
+  111
+    <NuxtPage />
+</template>
+
+<style>
+html,
+body,
+#__nuxt {
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+
+html.dark {
+ color-scheme: dark;
+}
+</style>
---
+++ ui/app/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const data = await $fetch('/api/payload')
+</script>
+
+<template>
+  <div>
+   {{ data }}
+  </div>
+</template>
